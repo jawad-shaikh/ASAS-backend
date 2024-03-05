b4d0b864eb22498464c41220f62867691ae9db87
jawad-shaikh <shaikhjawad314@gmail.com>
2024-03-06 02:34:45 +0500

    mostly done

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(express.static('uploads'));
 app.use(express.json({ limit: '100mb' }));
 app.use(cors());
-app.use(reqResInspector({ responseMessage: false }));
+// app.use(reqResInspector({ responseMessage: false }));
 
+app.get('/', (req, res) => res.send('hi'));
 app.use('/api/v1', require('./routes/index'));
 
 module.exports = app;
