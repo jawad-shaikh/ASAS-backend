917d8cfcf5913df03e233b7713b74f3ec3497589
jawad-shaikh <shaikhjawad314@gmail.com>
2024-03-10 01:54:08 +0500

    api added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const reqResInspector = require('express-req-res-inspector');
 
 const app = express();
@@ -11,5 +12,17 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('hi'));
 app.use('/api/v1', require('./routes/index'));
+app.get('/places', async (req, res) => {
+  try {
+    const { query } = req.query;
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=AIzaSyALid_clJdG76KwqFhqa5qvNqRb8dTt-`,
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = app;
